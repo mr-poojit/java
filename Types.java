742511d9667a3fcc0b90953fb026ed8e4c9ea909
poojit <poojitnagaloti611@gmail.com>
2024-06-01 17:13:22 +0530

    First commit

---
+++ Types.java
@@ -0,0 +1,15 @@
+public class Types {
+        public static void main(String[] args){
+            byte age= 20;
+            long viewsCount =3_123_456_789L;
+            float price = 10.99f;
+            char letter = 'P';
+            boolean isEligible = false;
+    
+            System.out.println("age: %d, viewsCount: %d, price: %.2f, letter: %c,  isEligible: %b%n"age,viewsCount,price,letter,isEligible);
+     
+        }
+        
+    }
+    
+    
---
+++ main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        int age=33;
+        System.out.println(age);
+    }
+}
